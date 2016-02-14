c91a0a1dd11acc0f40fc1ae6879d96538a0ae3ae
Teemu Salminen <teemu_s88@hotmail.com>
2016-02-14 19:07:07 +0200

    Remove edit/destroy from beerclub index

--- spec/features/beer_spec.rb
+++ spec/features/beer_spec.rb
@@ -3,12 +3,14 @@ include Helpers
 
 describe "Beer" do
   before :each do
+  	FactoryGirl.create :user
     sign_in(username:"Pekka", password:"Foobar1")
   end
 
   it "is added to database if name is valid" do
     visit new_beer_path
-    fill_in('beer[name]', with:'validi')
+    save_and_open_page
+    fill_in('beer_name', with:'validi')
 
     expect{
       click_button "Create Beer"
@@ -17,9 +19,9 @@ describe "Beer" do
 
   it "is not added to database if name is invalid" do
     visit new_beer_path
-    fill_in('beer[name]', with:'')
+    save_and_open_page
+    fill_in('beer_name', with:'')
     click_button "Create Beer"
-    #save_and_open_page
     expect(page).to have_content "Name can't be blank"
     expect(Beer.count).to eq(0)
   end
